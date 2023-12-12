08548e5c7a895260078d6799f81b3deabb631c0f
DeadshotLegend <shivanshggamer@gmail.com>
2023-12-11 20:12:27 -0800

    comitting changes

--- app.js
+++ app.js
@@ -1,29 +1,20 @@
-$(document).ready(function () {
-    // Make an AJAX request to the backend API endpoint
-    $.ajax({
-        url: '/api/poke',
-        method: 'GET',
-        success: function (data) {
-            // Handle the response data
-            displayPokemonList(data);
-        },
-        error: function (error) {
-            console.error('Error fetching Pokemon list:', error);
-        }
-    });
-});
-
-function displayPokemonList(data) {
-    // Assume the data structure is { "pokemon": [ {"name": "pokemon1"}, {"name": "pokemon2"}, ... ] }
-    var pokemonList = data.pokemon;
-
-    // Create HTML content to display the list
-    var htmlContent = '<ul>';
-    for (var i = 0; i < pokemonList.length; i++) {
-        htmlContent += '<li>' + pokemonList[i].name + '</li>';
-    }
-    htmlContent += '</ul>';
-
-    // Update the content of the #pokemonList div
-    $('#pokemonList').html(htmlContent);
-}
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Make a GET request to your Spring Boot backend
+    fetch('http://localhost:8080/api/poke/alphabet')
+      .then(response => response.json())
+      .then(data => {
+        const pokemonListElement = document.getElementById('pokemonList');
+  
+        // Iterate through the list and append each Pokemon name to the list
+        data.results.forEach(pokemon => {
+          const listItem = document.createElement('li');
+          listItem.textContent = pokemon.name;
+          pokemonListElement.appendChild(listItem);
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching Pokemon data:', error);
+      });
+  });
+  
