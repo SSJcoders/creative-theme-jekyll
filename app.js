5ee0dbc356f0adaf2f679ad4c826c51e986775cd
DeadshotLegend <shivanshggamer@gmail.com>
2023-12-11 20:24:12 -0800

    comitting changes

--- app.js
+++ app.js
@@ -2,19 +2,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Make a GET request to your Spring Boot backend
     fetch('http://localhost:8080/api/poke/alphabet')
-      .then(response => response.json())
-      .then(data => {
-        const pokemonListElement = document.getElementById('pokemonList');
-  
-        // Iterate through the list and append each Pokemon name to the list
-        data.results.forEach(pokemon => {
-          const listItem = document.createElement('li');
-          listItem.textContent = pokemon.name;
-          pokemonListElement.appendChild(listItem);
+        .then(response => response.json())
+        .then(data => {
+            const pokemonListElement = document.getElementById('pokemonList');
+
+            // Convert the entire data object to a JSON string
+            const jsonString = JSON.stringify(data, null, 2); // Add formatting for better readability (optional)
+
+            // Create a pre element to display the JSON data
+            const preElement = document.createElement('pre');
+            preElement.textContent = jsonString;
+
+            // Append the pre element to the parent element
+            pokemonListElement.appendChild(preElement);
+        })
+        .catch(error => {
+            console.error('Error fetching Pokemon data:', error);
         });
-      })
-      .catch(error => {
-        console.error('Error fetching Pokemon data:', error);
-      });
-  });
-  
+});
